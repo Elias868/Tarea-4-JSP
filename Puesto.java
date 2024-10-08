c01097429b1798d3b7baf6dc4115a6cc782cb699
Elias868 <josueeliasmonroymartinez34@gmail.com>
2024-10-07 22:36:31 -0600

    Update Puesto.java

--- Puesto.java
+++ Puesto.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 /**
  *
- * @author David Valenzuela
+ * @author Elias Monroy
  */
 public class Puesto {
     //para mantenimiento
