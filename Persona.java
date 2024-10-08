915ea111b91ba02024ffc7295767bce457a684f4
Elias868 <josueeliasmonroymartinez34@gmail.com>
2024-10-07 22:34:58 -0600

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 /**
  *
- * @author David Valenzuela
+ * @author Elias Monroy
  */
 abstract class Persona {
     private int id;
